11b48040d4af87b743316f9ecf046e89c4227868
Bryan-1065913 <1065913@hr.nl>
2025-09-24 08:50:25 +0200

    Change of header with new alt tag

--- frontend/src/components/common/Header/Header-Dashboard.tsx
+++ frontend/src/components/common/Header/Header-Dashboard.tsx
@@ -14,7 +14,7 @@ const HeaderDashboard = ({ open, onToggle }: Props) => {
                     <Nav className="d-none d-lg-flex">
                         <div className="dropdown text-end">
                             <a href="#" className="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                                <img src="/src/assets/images/default-user.svg" alt="mdo" width="32" height="32" className="rounded-circle"/>
+                                <img src="/src/assets/images/default-user.svg" alt="Default user icon" width="32" height="32" className="rounded-circle"/>
                             </a>
                             <ul className="dropdown-menu text-small">
                                 <li><a className="dropdown-item" href="#">New project...</a></li>
