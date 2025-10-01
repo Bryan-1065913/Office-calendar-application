3aa7574a4f4d0324de55f20174c75c0a1bc6a86a
Sem-1076101 <semjansen.langmuur@gmail.com>
2025-10-01 13:57:20 +0200

    setup loginresponse for testing login

---
+++ backend/OfficeCalendar.Api/models/LoginResponse.cs
@@ -0,0 +1,15 @@
+namespace OfficeCalendar.Api.Models;
+
+public class LoginResponse
+{
+    public string Token { get; set; } = string.Empty;
+    public UserDto User { get; set; } = new UserDto();
+}
+
+public class UserDto
+{
+    public string Id { get; set; } = string.Empty;
+    // public string FirstName { get; set; } = string.Empty;
+    // public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
