f25eb15e9cc25812e428353e12ca3590d54d57f0
Bryan-1065913 <1065913@hr.nl>
2025-10-04 10:27:35 +0200

    Add protected routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,27 +1,33 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from 'react-router';
+import { createBrowserRouter, RouterProvider } from 'react-router';
 
 import App from './App';
 import Home from './pages/Home/Home';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Events from './pages/Events/Events';
 import EventDetails from './pages/Events/EventDetail';
-import EventForm from './pages/Forms/EventForms.tsx'
+import EventForm from './pages/Forms/EventForms';
+import ProtectedRoute from './authentication/ProtectedRoute';
+
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
         children: [
             { index: true, element: <Home /> },
-            { path: 'dashboard', element: <Dashboard /> },
+
+            {
+                element: <ProtectedRoute role="Admin" />,
+                children: [
+                    { path: 'dashboard', element: <Dashboard /> },
+                    { path: 'eventform', element: <EventForm /> },
+                ],
+            },
+
             { path: 'events', element: <Events /> },
-            { path: 'events/:id', element: <EventDetails /> },        
-            { path: '/eventform', element: <EventForm /> },
+            { path: 'events/:id', element: <EventDetails /> },
         ],
     },
 ]);
