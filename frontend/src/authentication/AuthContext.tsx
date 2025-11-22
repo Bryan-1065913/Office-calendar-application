9f34d89aabe31a640780b7f8f1728bcd5fd957fe
Bryan de Knikker <1065913@hr.nl>
2025-11-22 15:09:41 +0100

    Changed Auth Context for User id

--- frontend/src/authentication/AuthContext.tsx
+++ frontend/src/authentication/AuthContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useState, useEffect, type ReactNode } from 'react';
 
 interface User {
-    id: number;
+    userId: number;
     email: string;
     firstName: string;
     lastName: string;
