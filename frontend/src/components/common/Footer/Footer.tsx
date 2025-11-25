edd4274eddcef29fc8974c42c0da086d4b83b1bd
Bryan de Knikker <1065913@hr.nl>
2025-11-25 08:29:26 +0100

    Refactored footer

--- frontend/src/components/common/Footer/Footer.tsx
+++ frontend/src/components/common/Footer/Footer.tsx
@@ -1,20 +1,19 @@
 // src/components/common/Footer/Footer.tsx
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import '../Footer/Footer.scss'
+import '../../../styles/General/Footer.css';
 
 const Footer: React.FC = () => {
     return (
-        <footer className="bg-dark text-light py-3 mt-auto">
-            <Container className="footer-logo">
-                <p className="mb-1 text-begin"><img className="mb-0 text-" src="/src/assets/images/logo.png" alt="Logo" width="105" height="50"/></p>
-            </Container>
-            <Container className="copyright">
-                <p className="mb-0 text-center">© 2025 Office Calendar App All right reserverd</p>
-            </Container>
-            <Container className="OPA">
-                <p className="mb-0 text-end">Privacy Police</p>
-            </Container>
+        <footer className="footer">
+            <div className="footer-row">
+                <div className="footer-logo">
+                    <img src="/src/assets/images/logo.png" alt="Logo" />
+                </div>
+
+                <p className="mb-0">© 2025 Office Calendar App All rights reserved</p>
+
+                <p className="mb-0">Privacy Policy</p>
+            </div>
         </footer>
     );
 };
