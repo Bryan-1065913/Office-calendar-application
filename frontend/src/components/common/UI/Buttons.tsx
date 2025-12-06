64e75ca00da88f27b6cfeafb32940b473d6faabf
Bryan de Knikker <1065913@hr.nl>
2025-12-06 17:39:13 +0100

    Chnaged buttons to accept type

--- frontend/src/components/common/UI/Buttons.tsx
+++ frontend/src/components/common/UI/Buttons.tsx
@@ -2,20 +2,24 @@
 import "../../../styles/UI/Button.css";
 import Chevron from "../../../assets/icons/chevron.svg?react";
 
-type ButtonVariant = "primary" | "secondary" | "dropdown";
+type ButtonVariant = "primary" | "secondary" | "dropdown" | "danger";
 
 type Props = {
     children?: React.ReactNode;
     variant?: ButtonVariant;
     onClick?: () => void;
     className?: string;
+    type?: "button" | "submit" | "reset";
+    disabled?: boolean;
 };
 
-const Button = ({ children, variant = "primary", onClick, className }: Props) => {
+const Button = ({ children, variant = "primary", onClick, className, type = "button", disabled }: Props) => {
     return (
         <button
             className={`oc-btn oc-btn-${variant} ${className ?? ""}`}
             onClick={onClick}
+            type={type}
+            disabled={disabled}
         >
             {children}
 
