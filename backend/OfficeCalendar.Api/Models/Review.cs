f6bd9f3e5ba0570d84610fb3a12267a9a1865499
ZakariaHogeschoolR <ztahiri26@gmail.com>
2025-12-11 12:52:47 +0100

    First version of making reviews work.

---
+++ backend/OfficeCalendar.Api/Models/Review.cs
@@ -0,0 +1,43 @@
+// Models/WorkStatus.cs
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OfficeCalendar.Api.Models
+{
+    [Table("work_status")]
+    public class Review
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("event_id")]
+        public int EventId { get; set; }
+
+        [Required]
+        [Column("user_id")]
+        public int UserId { get; set; }
+
+        [Required]
+        [Column("date")]
+        public DateTime Date { get; set; }
+        
+        [Required]
+        [Column("title_review")]
+        public string Title { get; set; }
+
+        [Required]
+        [Column("text_review")]
+        public string TextReview { get; set; }
+
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Column("updated_at")]
+        public DateTime? UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        [ForeignKey("UserId")]
+        public User? User { get; set; }
+    }
+}
--- frontend/src/components/common/Reviews/Reviews.tsx
+++ frontend/src/components/common/Reviews/Reviews.tsx
@@ -4,11 +4,16 @@ import {useAuth} from "../../../authentication/AuthContext";
 import NotFound from "../NotFound/NotFound";
 import RevieuwsRender from "./ReviewsRedering";
 import '../../../assets/fonts/sen.css';
+import Button from "../UI/Buttons";
+import { Navigate, useNavigate } from "react-router";
 
 const Revieuws = () => {
     const {user} = useAuth();
     const [input, setInput] = useState("");
+    const [title, setTitle] = useState("");
+    const [textLength, setTextLength] = useState(0);
     const [saveTextList, setSaveTextList] = useState<string[]>([]);
+    const navigate = useNavigate();
     if(user == null)
     {
         return <NotFound/>
@@ -17,22 +22,46 @@ const Revieuws = () => {
     {
         return <NotFound/>
     }
+
+    const titleInput = (input: string) => {
+        // Hier kun je extra logica toevoegen als dat nodig is
+        input.split(".")[0];
+        if(input.length > 10) 
+        {
+            return input.charAt(0).toUpperCase() + input.slice(1);
+        }
+        return input;
+    }
+
     const saveText = (e: React.MouseEvent) => {
+        if(textLength > 500 || textLength < 150) {
+            e.preventDefault();
+            return;
+        }
         e.preventDefault();
+        const titleInputValue = titleInput(input);
+        setTitle(titleInputValue);
         setSaveTextList(prev => [...prev, input]);
+        setTextLength(0);
+        setTitle("");
         setInput("");
     }   
 
     return(
         <>
             <div className="Main-wrapper">
+                <Button className="Main-Button" variant="primary" onClick={() => navigate(`/events/`)}>&nbsp;&nbsp;&nbsp;&nbsp;Terug&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Button>
                 <form className="formulier-Revieuw">  
                     <textarea
                         value={input}
-                        onChange={(e) => setInput(e.target.value)}
+                        onChange={(e) => { setInput(e.target.value); setTextLength(e.target.value.length); }}
                         placeholder="Type here your revieuw"
                         className="revieuw-textarea"
                     />
+                    {textLength > 500 || textLength < 150 ? (
+                        <div className="Warning-Message">Warning you either cant have less than 150 or more than 500 characters</div>
+                    ) : null}
+                    <div className="text-length">{textLength} / 500</div>
                     <button className="submit-button" onClick={saveText}><img src="/src/assets/icons/verzend.png" alt="Logo" width="50" height="50"/></button>
                 </form>
             </div>
